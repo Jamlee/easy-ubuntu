02683e634df8a2cf392480809c89e5a8190ae2ec
jamlee <1575103047.com>
2018-11-11 21:51:14 +0800

    添加 redirect 操作

--- src/backend/server.clj
+++ src/backend/server.clj
@@ -1,19 +1,26 @@
 (ns backend.server
-  (:require [liberator.core :refer [resource]]
+  (:require [liberator.core :refer [resource defresource]]
     [ring.middleware.file :refer [wrap-file]]
     [ring.middleware.params :refer [wrap-params]]
     [compojure.route :as route]
-    [compojure.core :refer [defroutes ANY]]))
+    [compojure.core :refer [defroutes GET ANY]]))
+
 
 ;; 定义请求入口
 (defroutes app
+  (ANY "/" [] (resource :available-media-types ["text/html"]
+                        :exists? false :existed? true
+                        :moved-permanently? true
+                        :location "/index.html"))
   (ANY "/foo" [] (resource :available-media-types ["text/html"]
                            :handle-ok "hello world"))                                    
   (ANY "/bar" [] (resource :available-media-types ["text/html"]
                            :handle-ok (fn [ctx]
                                         (format "<html>It's %d milliseconds since the beginning of the epoch."
                                                 (System/currentTimeMillis)))))
-  (route/files "public")
+
+  ;; 服务静态文件
+  (route/resources "/" {:root "public"})
   (route/not-found "Not found."))
 
 (def handler 
