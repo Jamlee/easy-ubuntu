20b3ca6a2424b1104fb3480ad4d0913704ac831c
jamlee <1575103047.com>
2018-11-11 18:51:53 +0800

    添加 服务端 handler

--- src/backend/server.clj
+++ src/backend/server.clj
@@ -3,15 +3,14 @@
     [ring.middleware.params :refer [wrap-params]]
     [compojure.core :refer [defroutes ANY]]))
 
-(defroutes app
-  (ANY "/" [] (resource)))
-
+;; 定义请求入口
 (defroutes app
   (ANY "/foo" [] (resource :available-media-types ["text/html"]
+                           :handle-ok "hello world"))                                    
+  (ANY "/bar" [] (resource :available-media-types ["text/html"]
                            :handle-ok (fn [ctx]
                                         (format "<html>It's %d milliseconds since the beginning of the epoch."
                                                 (System/currentTimeMillis))))))
 
 (def handler 
-  (-> app 
-    wrap-params))
+  (-> app wrap-params))
